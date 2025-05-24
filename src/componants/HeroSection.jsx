0e8f6b7353a45ab3853d537d8eea65762d7a4f6e
prasannvyawahare <32096314+prasannvyawahare@users.noreply.github.com>
2025-05-24 11:16:07 -0400

    update hero section

--- src/componants/HeroSection.jsx
+++ src/componants/HeroSection.jsx
@@ -25,17 +25,6 @@ const HeroSection = () => {
                     View Our Work
                 </a>
             </div>
-            {/* <div className="flex mt-10 justify-center">
-                <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400
-                mx-2 my-4"> <source src={video1} type="video/mp4"/>
-                 Your browser not support video tag.
-                </video>
-                <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400
-                mx-2 my-4"> <source src={video2} type="video/mp4"/>
-                 Your browser not support video tag.
-                </video>
-            </div> */}
-    
     </div>
 
 
